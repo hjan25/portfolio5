eb50c016b6b16f767a93f958da43d3b2081a1a47
hjan25 <ahj531@gmail.com>
2021-01-16 19:20:43 +0900

    Create project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// main js
+
+// test
+(() => {
+    /* document.querySelector('.header').addEventListener('click', function() {
+        document.querySelector('.header .row').classList.add('on');
+    }) 스크롤시 */
+})();
